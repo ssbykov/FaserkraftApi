e86dd16ce4d7d6df0da2247d236c33ebee63df67
Sergei Bykov <ss.bykoff@gmail.com>
2025-11-05 12:55:10 +0300

    добавлен метод по созданию нового продукта

---
+++ app/database/crud/products.py
@@ -0,0 +1,39 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.database import Product, ProductStep, SessionDep
+from app.database.crud.mixines import GetBackNextIdMixin
+from app.database.crud.processes import ProcessRepository
+from app.database.models.product_step import StepStatus
+from app.database.schemas.product import ProductCreate
+
+
+def get_product_repo(session: SessionDep) -> "ProductRepository":
+    return ProductRepository(session)
+
+
+class ProductRepository(GetBackNextIdMixin[Product]):
+    session: AsyncSession
+    model = Product
+
+    async def create_product(self, product_in: ProductCreate):
+        # 1️⃣ Create product
+
+        product = product_in.to_orm()
+        self.session.add(product)
+        await self.session.flush()
+        product_id = product.id
+
+        # 2️⃣ Create all steps for this product
+        repo = ProcessRepository(self.session)
+        process = await repo.get(product_in.process_id)
+        for step_def in process.steps:
+            ps = ProductStep(
+                product_id=product_id,
+                step_definition_id=step_def.id,
+                status=StepStatus.pending,
+            )
+            self.session.add(ps)
+
+        await self.session.commit()
+        await self.session.refresh(product)
+        return product
