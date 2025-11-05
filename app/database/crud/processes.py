690a855aa07a710b5b60f2f70dcd7c0a2e33c9e0
Sergei Bykov <ss.bykoff@gmail.com>
2025-11-05 12:56:00 +0300

    добавлен метод по получению процесса из базы

--- app/database/crud/processes.py
+++ app/database/crud/processes.py
@@ -1,6 +1,23 @@
+from fastapi import HTTPException
+from sqlalchemy import select
+from sqlalchemy.orm import selectinload
+
 from app.database import Process
 from app.database.crud.mixines import GetBackNextIdMixin
 
 
 class ProcessRepository(GetBackNextIdMixin[Process]):
     model = Process
+
+    async def get(self, process_id: int) -> Process:
+        stmt = (
+            select(self.model)
+            .options(selectinload(self.model.steps))  # stages — имя relationship поля
+            .where(self.model.id == process_id)
+        )
+        process = await self.session.scalar(stmt)
+        if process:
+            return process
+        raise HTTPException(
+            status_code=404, detail=f"Процесс с id {process_id} не найден"
+        )
