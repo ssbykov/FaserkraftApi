17aaf3c6cd3032260292c62bdeba0d6e3ea8169d
Sergei Bykov <ss.bykoff@gmail.com>
2025-11-13 15:03:26 +0300

    добавлен роут get_product

--- app/api/api_v1/products.py
+++ app/api/api_v1/products.py
@@ -3,9 +3,11 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 from starlette import status
 
+from app.api.api_v1.fastapi_users import current_super_user
 from app.core import settings
 from app.database import Product
 from app.database.crud.products import ProductRepository, get_product_repo
+from app.database.models import User
 from app.database.schemas.product import ProductRead, ProductCreate
 
 router = APIRouter(
@@ -21,9 +23,24 @@ router.include_router(
 async def create_product(
     product_in: ProductCreate,
     repo: Annotated[ProductRepository, Depends(get_product_repo)],
+    user: Annotated[User, Depends(current_super_user)],
 ) -> Product | str:
     try:
         product = await repo.create_product(product_in)
         return product
+    except Exception as e:
+        return f"Произошла ошибка: {e}"
+
+
+@router.get(
+    "/{serial_number}", response_model=ProductRead, status_code=status.HTTP_200_OK
+)
+async def get_product(
+    serial_number: str,
+    repo: Annotated[ProductRepository, Depends(get_product_repo)],
+    user: Annotated[User, Depends(current_super_user)],
+):
+    try:
+        return await repo.get(serial_number)
     except Exception as e:
         return f"Произошла ошибка: {e}"
