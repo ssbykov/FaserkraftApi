f639b2e2d36ecae0f14e9d2ec852aabdd3fb4fa9
Sergei Bykov <ss.bykoff@gmail.com>
2025-11-13 15:02:47 +0300

    добавлен метод get

--- app/database/crud/products.py
+++ app/database/crud/products.py
@@ -1,6 +1,9 @@
+from fastapi import HTTPException
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload, joinedload
 
-from app.database import Product, ProductStep, SessionDep
+from app.database import Product, ProductStep, SessionDep, StepDefinition
 from app.database.crud.mixines import GetBackNextIdMixin
 from app.database.crud.processes import ProcessRepository
 from app.database.models.product_step import StepStatus
@@ -37,3 +40,20 @@ class ProductRepository(GetBackNextIdMixin[Product]):
         await self.session.commit()
         await self.session.refresh(product)
         return product
+
+    async def get(self, serial_number: str) -> Product:
+        stmt = (
+            select(self.model)
+            .options(
+                joinedload(self.model.steps)
+                .joinedload(ProductStep.step_definition)
+                .joinedload(StepDefinition.template)
+            )
+            .where(self.model.serial_number == serial_number)
+        )
+        product = await self.session.scalar(stmt)
+        if product:
+            return product
+        raise HTTPException(
+            status_code=404, detail=f"Продукт с id {serial_number} не найден"
+        )
