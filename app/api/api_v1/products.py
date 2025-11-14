bebf1580f26b7a8cb4f6a24076ade0402a37acfb
Sergei Bykov <ss.bykoff@gmail.com>
2025-11-14 20:31:55 +0300

    добавлена схема ProductCreateOut, улучшена обработка ошибок

--- app/api/api_v1/products.py
+++ app/api/api_v1/products.py
@@ -1,14 +1,14 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.exc import IntegrityError
 from starlette import status
 
 from app.api.api_v1.fastapi_users import current_super_user
 from app.core import settings
-from app.database import Product
 from app.database.crud.products import ProductRepository, get_product_repo
 from app.database.models import User
-from app.database.schemas.product import ProductRead, ProductCreate
+from app.database.schemas.product import ProductRead, ProductCreate, ProductCreateOut
 
 router = APIRouter(
     tags=["Products"],
@@ -19,17 +19,35 @@ router.include_router(
 )
 
 
-@router.post("/", response_model=ProductRead, status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=ProductCreateOut, status_code=status.HTTP_201_CREATED)
 async def create_product(
     product_in: ProductCreate,
     repo: Annotated[ProductRepository, Depends(get_product_repo)],
     user: Annotated[User, Depends(current_super_user)],
-) -> Product | str:
+) -> ProductCreateOut | str:
     try:
         product = await repo.create_product(product_in)
-        return product
+        return ProductCreateOut.model_validate(product)
+    except IntegrityError as e:
+        msg = str(e.orig)
+        if (
+            "violates foreign key constraint" in msg
+            or "ForeignKeyViolationError" in msg
+        ):
+            raise HTTPException(
+                status_code=422, detail="Указан несуществующий process_id"
+            )
+        elif "duplicate key value" in msg or "уже существует" in msg:
+            raise HTTPException(
+                status_code=409,
+                detail="Продукт с таким серийным номером уже существует",
+            )
+        else:
+            # Остальные ошибки целостности
+            raise HTTPException(status_code=400, detail=f"Ошибка данных: {msg}")
     except Exception as e:
-        return f"Произошла ошибка: {e}"
+        # Прочие ошибки — 500 Internal Server Error
+        raise HTTPException(status_code=500, detail=f"Произошла ошибка: {e}")
 
 
 @router.get(
@@ -43,4 +61,4 @@ async def get_product(
     try:
         return await repo.get(serial_number)
     except Exception as e:
-        return f"Произошла ошибка: {e}"
+        raise HTTPException(status_code=500, detail=f"Произошла ошибка: {e}")
