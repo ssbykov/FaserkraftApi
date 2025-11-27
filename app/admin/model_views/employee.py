5dada3e87f98d1777ce0548867a0766e7f428f18
Sergei Bykov <ss.bykoff@gmail.com>
2025-11-27 13:08:34 +0300

    перенес action "generate-qr"

--- app/admin/model_views/employee.py
+++ app/admin/model_views/employee.py
@@ -1,5 +1,10 @@
+from sqladmin import action
+from starlette.requests import Request
+from starlette.responses import RedirectResponse
+
 from app.admin.custom_model_view import CustomModelView
-from app.database import Employee
+from app.core.auth.user_manager_helper import UserManagerHelper
+from app.database import Employee, db_helper
 from app.database.crud.employees import EmployeeRepository
 
 
@@ -34,3 +39,24 @@ class EmployeeAdmin(
     ]
 
     can_export = False
+
+    action_in_header = ["generate-qr"]
+
+    @action(
+        name="generate-qr",
+        label="QR-код",
+        add_in_detail=True,
+        add_in_list=False,
+        confirmation_message=f"Cоздать QR-код для пользователя?",
+    )
+    async def generate_qr(self, request: Request) -> RedirectResponse:
+        pks = int(request.query_params["pks"])
+        async for session in db_helper.get_session():
+            repo = self.repo_type(session)
+            employee = await repo.get_by_id(obj_id=pks)
+
+            user_manager_helper = UserManagerHelper()
+            user = await user_manager_helper.get_user_by_id(user_id=employee.user_id)
+            await user_manager_helper.forgot_password(user=user, request=request)
+
+        return RedirectResponse(request.url_for("admin:list", identity=self.identity))
--- app/admin/model_views/user.py
+++ app/admin/model_views/user.py
@@ -1,19 +1,9 @@
-import inspect
-import io
-
-import qrcode
-from sqladmin import action
 from starlette.requests import Request
-from starlette.responses import RedirectResponse
 
 from app.admin.custom_model_view import CustomModelView
 from app.admin.utils import check_superuser
-from app.core import config
-from app.core.auth.access_tokens_helper import AccessTokensHelper
-from app.core.auth.user_manager_helper import UserManagerHelper
 from app.database.crud.users import UsersRepository
 from app.database.models import User
-from app.tasks import run_process_mail
 
 
 class UserAdmin(
@@ -55,38 +45,3 @@ class UserAdmin(
 
     def is_accessible(self, request: Request) -> bool:
         return check_superuser(request)
-
-    action_in_header = ["generate-qr"]
-
-    @action(
-        name="generate-qr",
-        label="QR-код",
-        add_in_detail=True,
-        add_in_list=False,
-        confirmation_message=f"Cоздать QR-код для пользователя?",
-    )
-    async def generate_qr(self, request: Request) -> RedirectResponse:
-        pks = int(request.query_params["pks"])
-        user_manager_helper = UserManagerHelper()
-        user = await user_manager_helper.get_user_by_id(user_id=pks)
-        access_tokens_helper = AccessTokensHelper()
-        token = await access_tokens_helper.write_token(user=user)
-
-        url = (
-            f"http://{config.settings.run.host}"
-            f":{config.settings.run.port}"
-            f"/users/me"
-        )
-        qr_json = {"url": url, "token": token}
-
-        img = qrcode.make(qr_json)
-        byte_io = io.BytesIO()
-        img.save(byte_io, format="PNG")
-        byte_io.seek(0)
-        context = {
-            "name": user.email,
-            "user_email": user.email,
-            "qr_code_bytes": byte_io.getvalue(),
-        }
-        run_process_mail.delay(None, context=context, action="send_qr")
-        return RedirectResponse(request.url_for("admin:list", identity=self.identity))
