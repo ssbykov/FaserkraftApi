5f4217bdc1768af3d2e7bf7cfef8f50e6a7b0a22
Sergei Bykov <ss.bykoff@gmail.com>
2025-11-27 12:59:17 +0300

    изменена логика проверки ошибок

--- app/database/crud/devices.py
+++ app/database/crud/devices.py
@@ -1,19 +1,27 @@
 from app.database import SessionDep
 from app.database.crud.mixines import GetBackNextIdMixin
 from app.database.models import Device
-from app.database.schemas.device_schema import DeviceCreate
+from app.database.schemas.device import DeviceCreate
 
 
-def get_product_repo(session: SessionDep) -> "DeviceRepository":
+def get_device_repo(session: SessionDep) -> "DeviceRepository":
     return DeviceRepository(session)
 
 
 class DeviceRepository(GetBackNextIdMixin[Device]):
     model = Device
 
-    async def create_device(self, device_in: DeviceCreate):
+    async def create_device(self, device_in: DeviceCreate) -> Device:
         device = device_in.to_orm()
         self.session.add(device)
-        await self.session.commit()
+
+        try:
+            await self.session.flush()
+            await self.session.commit()
+        except Exception as e:
+            print(e)
+            await self.session.rollback()
+            raise
+
         await self.session.refresh(device)
         return device
