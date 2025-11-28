ad9e47bc1682360d263854efce6c87e86792a541
Sergei Bykov <ss.bykoff@gmail.com>
2025-11-28 15:07:12 +0300

    исправлена ошибка параметра на device_id

---
+++ app/alembic/versions/2025_11_28_1443-45e28112c2e4_добавлена_device.py
@@ -0,0 +1,56 @@
+"""добавлена Device
+
+Revision ID: 45e28112c2e4
+Revises: 6ed58a8bf1bb
+Create Date: 2025-11-28 14:43:27.838644
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "45e28112c2e4"
+down_revision: Union[str, None] = "6ed58a8bf1bb"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "devices",
+        sa.Column("device_id", sa.String(), nullable=False),
+        sa.Column("model", sa.String(), nullable=False),
+        sa.Column("manufacturer", sa.String(), nullable=False),
+        sa.Column("is_active", sa.Boolean(), nullable=True),
+        sa.Column(
+            "created_at",
+            sa.DateTime(timezone=True),
+            server_default=sa.text("now()"),
+            nullable=True,
+        ),
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.PrimaryKeyConstraint("id", name=op.f("pk_devices")),
+        sa.UniqueConstraint("device_id", name=op.f("uq_devices_device_id")),
+    )
+    op.create_index(op.f("ix_devices_id"), "devices", ["id"], unique=True)
+    op.add_column("employees", sa.Column("device_id", sa.Integer(), nullable=True))
+    op.create_foreign_key(
+        op.f("fk_employees_device_id_devices"),
+        "employees",
+        "devices",
+        ["device_id"],
+        ["id"],
+    )
+
+
+def downgrade() -> None:
+    op.drop_constraint(
+        op.f("fk_employees_device_id_devices"), "employees", type_="foreignkey"
+    )
+    op.drop_column("employees", "device_id")
+    op.drop_index(op.f("ix_devices_id"), table_name="devices")
+    op.drop_table("devices")
