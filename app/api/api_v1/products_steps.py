0de2ca61a033f69e3872c6079e47d9e6bcc027a4
Sergei Bykov <ss.bykoff@gmail.com>
2025-12-05 14:50:22 +0300

    изменена логика accept_step

--- app/api/api_v1/products_steps.py
+++ app/api/api_v1/products_steps.py
@@ -4,11 +4,12 @@ from fastapi import APIRouter, Depends, HTTPException
 from starlette import status
 
 from app.core import settings
+from app.database.crud.employees import EmployeeRepository, get_employee_repo
 from app.database.crud.products_steps import (
     ProductStepRepository,
     get_products_steps_repo,
 )
-from app.database.schemas.product_step import ProductStepUpdate
+from app.database.schemas.product_step import ProductStepUpdate, ProductStepBase
 
 router = APIRouter(
     tags=["Products_Steps"],
@@ -19,17 +20,18 @@ router.include_router(
 )
 
 
-@router.post("/", response_model=ProductStepUpdate, status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=ProductStepBase, status_code=status.HTTP_201_CREATED)
 async def accept_step(
-    step_id: int,
-    employee_id: int,
+    step: ProductStepUpdate,
     repo: Annotated[ProductStepRepository, Depends(get_products_steps_repo)],
-) -> ProductStepUpdate:
+    employee_repo: Annotated[EmployeeRepository, Depends(get_employee_repo)],
+) -> ProductStepBase:
     try:
-        step = await repo.accept_step(step_id, employee_id)
+        employee_id = await employee_repo.get_employee_id_by_email(step.performed_by)
+        step = await repo.accept_step(step.id, employee_id)
         if step is None:
             raise HTTPException(status_code=404, detail="Шаг не найден")
-        return ProductStepUpdate.model_validate(step)
+        return ProductStepBase.model_validate(step)
     except HTTPException:
         raise
     except Exception as e:
