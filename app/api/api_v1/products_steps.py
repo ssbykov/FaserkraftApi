297f2f01b0dc4f3bd67abe842ff6f085446cae03
Sergei Bykov <ss.bykoff@gmail.com>
2026-02-09 13:46:41 +0300

    добавлен метод check_step_in_daily_plan

--- app/api/api_v1/products_steps.py
+++ app/api/api_v1/products_steps.py
@@ -1,3 +1,4 @@
+from datetime import date
 from typing import Annotated, Optional
 
 from fastapi import APIRouter, Depends, HTTPException
@@ -5,6 +6,7 @@ from starlette import status
 
 from app.api.api_v1.fastapi_users import current_user
 from app.core import settings
+from app.database.crud.daily_plans import DailyPlanRepository, get_daily_plan_repo
 from app.database.crud.employees import EmployeeRepository, get_employee_repo
 from app.database.crud.products import get_product_repo, ProductRepository
 from app.database.crud.products_steps import (
@@ -28,12 +30,26 @@ router.include_router(
 async def accept_step(
     step_id: int,
     repo: Annotated[ProductStepRepository, Depends(get_products_steps_repo)],
+    day_plan_repo: Annotated[DailyPlanRepository, Depends(get_daily_plan_repo)],
     employee_repo: Annotated[EmployeeRepository, Depends(get_employee_repo)],
     product_repo: Annotated[ProductRepository, Depends(get_product_repo)],
     user: Annotated[User, Depends(current_user)],
+    plan_date: date | None = None,
 ) -> Optional[ProductRead]:
     try:
-        employee = await employee_repo.get_by_id(user.id)
+        employee = await employee_repo.get_by_user_id(user.id)
+        if plan_date is None:
+            plan_date = date.today()
+
+        if not await day_plan_repo.check_step_in_daily_plan(
+            date=plan_date,
+            employee_id=employee.id,
+            product_step_id=step_id,
+        ):
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="В планах нет этого этапа"
+            )
+
         step = await repo.accept_step(step_id, employee.id)
         if step is None:
             raise HTTPException(status_code=404, detail="Шаг не найден")
--- app/database/crud/daily_plans.py
+++ app/database/crud/daily_plans.py
@@ -1,9 +1,9 @@
 from datetime import date as date_type
 
-from sqlalchemy import select
+from sqlalchemy import select, exists
 from sqlalchemy.orm import joinedload
 
-from app.database import DailyPlan, SessionDep, StepDefinition
+from app.database import DailyPlan, SessionDep, StepDefinition, ProductStep
 from app.database.crud.mixines import GetBackNextIdMixin
 from app.database.models import DailyPlanStep
 
@@ -37,3 +37,31 @@ class DailyPlanRepository(GetBackNextIdMixin[DailyPlan]):
 
         result = await self.session.execute(stmt)
         return result.unique().scalars().all()
+
+    async def check_step_in_daily_plan(
+        self,
+        *,
+        date: date_type,
+        employee_id: int,
+        product_step_id: int,
+    ):
+        # 1. subquery на step_definition_id нужного ProductStep
+        step_def_id_stmt = select(ProductStep.step_definition_id).where(
+            ProductStep.id == product_step_id
+        )
+        step_def_id_subq = step_def_id_stmt.scalar_subquery()
+
+        # 2. EXISTS по DailyPlan + DailyPlanStep с таким же step_definition_id
+        subq = (
+            select(DailyPlanStep.id)
+            .join(DailyPlan, DailyPlanStep.daily_plan_id == DailyPlan.id)
+            .where(
+                DailyPlan.employee_id == employee_id,
+                DailyPlan.date == date,
+                DailyPlanStep.step_definition_id == step_def_id_subq,
+            )
+        )
+
+        stmt = select(exists(step_def_id_subq))
+        result = await self.session.execute(stmt)
+        return bool(result.scalar())
