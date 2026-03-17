213f46e1b263071f37acccaff99cb5aaca857d48
Sergei Bykov <ss.bykoff@gmail.com>
2026-03-17 19:18:08 +0300

    добавил products в PackagingRead

--- app/database/schemas/packaging.py
+++ app/database/schemas/packaging.py
@@ -3,10 +3,10 @@ from typing import List, ClassVar, Type
 from zoneinfo import ZoneInfo
 
 from pydantic import Field
-from sqlalchemy import null
 
 from app.database import BaseSchema
 from app.database.models import Packaging
+from app.database.schemas.product import ProductsFinishedRead
 
 
 class PackagingBase(BaseSchema):
@@ -30,6 +30,7 @@ class PackagingCreateWithProducts(PackagingCreate):
 
 class PackagingRead(PackagingCreate):
     id: int
+    products: List[ProductsFinishedRead]
 
     class Config:
         from_attributes = True
