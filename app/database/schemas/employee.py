e75a4448e3b919f27b620cddab58b55ad5dc55b2
Sergei Bykov <ss.bykoff@gmail.com>
2026-03-22 13:56:20 +0300

    добавил параметр user

--- app/database/schemas/employee.py
+++ app/database/schemas/employee.py
@@ -1,8 +1,10 @@
 from app.database import BaseSchema
+from database.schemas.user import UserRead
 
 
 class EmployeeRead(BaseSchema):
     id: int
     name: str
+    user: UserRead
 
     model_config = {"from_attributes": True}
