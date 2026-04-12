7cf9d349ce02448050ae39017eb22be32f32ede1
Sergei Bykov <ss.bykoff@gmail.com>
2026-04-12 19:52:56 +0300

    оптимизированы роуты с использованием get_current_employee и require_admin_or_master

--- app/api/api_v1/employees.py
+++ app/api/api_v1/employees.py
@@ -1,14 +1,11 @@
-from typing import List, Annotated, Sequence
+from typing import List, Annotated
 
 from fastapi import APIRouter, HTTPException, Depends
 from starlette import status
 
-from app.api.api_v1.fastapi_users import current_user
+from app.api.api_v1.dependencies import require_admin_or_master
 from app.core import settings
-from app.database import Employee
 from app.database.crud.employees import EmployeeRepository, get_employee_repo
-from app.database.models import User
-from app.database.models.employee import Role
 from app.database.schemas.employee import EmployeeRead
 
 router = APIRouter(
@@ -24,31 +21,16 @@ router = APIRouter(
 )
 async def get_employees(
     repo: Annotated[EmployeeRepository, Depends(get_employee_repo)],
-    employee_repo: Annotated[EmployeeRepository, Depends(get_employee_repo)],
-    user: Annotated[User, Depends(current_user)],
-) -> Sequence[Employee] | None:
+    employee: Annotated[EmployeeRead, Depends(require_admin_or_master)],
+) -> List[EmployeeRead]:
     try:
-        employee = await employee_repo.get_by_user_id(user.id)
-
-        if employee is None:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail="Профиль сотрудника для данного пользователя не найден",
-            )
-
-        if employee.role in [Role.admin, Role.master]:
-            return await repo.get_all()
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Нет прав доступа к этому ресурсу",
-        )
+        employees = await repo.get_all()
+        return [EmployeeRead.model_validate(e) for e in employees]
 
     except HTTPException as exc:
-        # пробрасываем 404 и другие осознанные HTTP-ошибки
         raise exc
     except Exception:
-        # внутренняя ошибка без лишних деталей наружу
         raise HTTPException(
             status_code=500,
-            detail="Произошла внутренняя ошибка при получении продукта",
+            detail="Произошла внутренняя ошибка при получении списка сотрудников",
         )
