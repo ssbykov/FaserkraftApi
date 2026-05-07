83d03cbbd5b365e687f71b9e10be9356ddcf687b
Sergei Bykov <ss.bykoff@gmail.com>
2026-05-07 12:51:38 +0300

    добавил возвращаемый список сотрудников в зависимости от роли

--- app/api/api_v1/employees.py
+++ app/api/api_v1/employees.py
@@ -3,10 +3,11 @@ from typing import List, Annotated
 from fastapi import APIRouter, HTTPException, Depends
 from starlette import status
 
-from app.api.api_v1.dependencies import require_admin_or_master
+from app.api.api_v1.dependencies import get_current_employee
 from app.core import settings
 from app.database.crud.employees import EmployeeRepository, get_employee_repo
 from app.database.schemas.employee import EmployeeRead
+from database.models.employee import Role
 
 router = APIRouter(
     tags=["Employees"],
@@ -21,10 +22,13 @@ router = APIRouter(
 )
 async def get_employees(
     repo: Annotated[EmployeeRepository, Depends(get_employee_repo)],
-    employee: Annotated[EmployeeRead, Depends(require_admin_or_master)],
+    employee: Annotated[EmployeeRead, Depends(get_current_employee)],
 ) -> List[EmployeeRead]:
     try:
-        employees = await repo.get_all()
+        if employee.role == Role.master:
+            employees = await repo.get_all()
+        else:
+            employees = [await repo.get_by_id(employee.id)]
         return [EmployeeRead.model_validate(e) for e in employees]
 
     except HTTPException as exc:
