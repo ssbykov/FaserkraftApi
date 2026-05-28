7493cf4cb603c5d41cdd2f1bf353ffb6105e686b
Sergei Bykov <ss.bykoff@gmail.com>
2026-05-28 20:01:03 +0300

    добавил параметр model_config в базовую схему

--- app/database/schemas/process.py
+++ app/database/schemas/process.py
@@ -1,19 +1,11 @@
-from typing import List
-
 from app.database import BaseSchema
 from app.database.schemas.size_type import SizeType
-from app.database.schemas.step_definition import StepDefinitionRead
 
-class ProcessReadBase(BaseSchema):
+
+class ProcessBase(BaseSchema):
     id: int
     name: str
+
+
+class ProcessReadShort(ProcessBase):
     size_type: SizeType
-    model_config = {"from_attributes": True}
-
-
-
-class ProcessRead(ProcessReadBase):
-    description: str | None = None
-    steps: List[StepDefinitionRead]
-
-    model_config = {"from_attributes": True}
