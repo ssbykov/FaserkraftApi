5d05525a30edaab05cb0873db5e76d74e4c9940d
Sergei Bykov <ss.bykoff@gmail.com>
2026-08-20 09:07:12 +0300

    Добавил в данные статистики типоразмеры

--- app/database/schemas/statistics.py
+++ app/database/schemas/statistics.py
@@ -4,6 +4,8 @@ from pydantic import BaseModel
 class StepCountStatRead(BaseModel):
     process_id: int
     process_name: str
+    size_type_id: int | None = None
+    size_type_name: str | None = None
     step_definition_id: int
     order: int
     step_name: str
