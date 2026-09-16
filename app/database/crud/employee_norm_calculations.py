32fb9d90fa856c1b1fe01c0083b98956e0997059
Sergei Bykov <ss.bykoff@gmail.com>
2026-09-16 13:28:35 +0300

    добавил метод get_working_days_by_employee

--- app/database/crud/employee_norm_calculations.py
+++ app/database/crud/employee_norm_calculations.py
@@ -1,6 +1,9 @@
-from app.database import SessionDep
+from collections import defaultdict
+
+from sqlalchemy import select
+
+from app.database import EmployeeNormCalculation, SessionDep
 from app.database.crud.mixines import GetBackNextIdMixin
-from app.database.models import EmployeeNormCalculation
 
 
 def get_employee_norm_calculation_repo(
@@ -11,3 +14,31 @@ def get_employee_norm_calculation_repo(
 
 class EmployeeNormCalculationRepository(GetBackNextIdMixin[EmployeeNormCalculation]):
     model = EmployeeNormCalculation
+
+    async def get_norms_for_steps(
+        self,
+        step_definition_ids: set[int],
+    ) -> dict[int, list[EmployeeNormCalculation]]:
+        """
+        Возвращает по каждому step_definition_id список записей норм,
+        отсортированный по дате (для построения интервалов действия нормы
+        в app.services.norm_calculation.build_norm_intervals).
+        """
+        if not step_definition_ids:
+            return {}
+
+        stmt = (
+            select(EmployeeNormCalculation)
+            .where(EmployeeNormCalculation.step_definition_id.in_(step_definition_ids))
+            .order_by(
+                EmployeeNormCalculation.step_definition_id,
+                EmployeeNormCalculation.date,
+            )
+        )
+        result = await self.session.execute(stmt)
+        rows = result.scalars().all()
+
+        grouped: dict[int, list[EmployeeNormCalculation]] = defaultdict(list)
+        for row in rows:
+            grouped[row.step_definition_id].append(row)
+        return grouped
